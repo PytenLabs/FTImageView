d92fe61d8c13dcbba4146ae3aa45c8ac8cb0a493
Amrutha <amrutha@pytenlabs.com>
2016-09-29 15:21:02 +0530

    Add image transform(Circle)

--- blackwhiteimageview/src/main/java/com/pytenlabs/blackwhiteimageview/BlackWhiteImageView.java
+++ blackwhiteimageview/src/main/java/com/pytenlabs/blackwhiteimageview/BlackWhiteImageView.java
@@ -10,6 +10,8 @@ import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v8.renderscript.Allocation;
 import android.support.v8.renderscript.Element;
 import android.support.v8.renderscript.RenderScript;
@@ -30,6 +32,8 @@ public class BlackWhiteImageView extends ImageView {
     Boolean mChangeTouchColor = false;
     Boolean mChangeToBlacknWhite = false;
     Boolean mIsBlur = false;
+    String mFilter = null, mTransform = null;
+    String BW = "bw", BLUR = "blur";
 
     Canvas mCanvas;
 
@@ -45,9 +49,9 @@ public class BlackWhiteImageView extends ImageView {
         super(context, attrs, defStyleAttr);
 
         getAttributes(context, attrs, defStyleAttr);
-        setBlacknWhite();
         setListener();
-        setBlurListener();
+        setFilter();
+        setTransform();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -55,19 +59,29 @@ public class BlackWhiteImageView extends ImageView {
         super(context, attrs, defStyleAttr, defStyleRes);
 
         getAttributes(context, attrs, defStyleAttr);
-        setBlacknWhite();
         setListener();
-        setBlurListener();
+        setFilter();
+        setTransform();
 
     }
 
-    private void setBlurListener() {
-        if (mIsBlur) {
+    private void setTransform() {
+        if (mTransform.equals("0")){
+            createCircularImageBitmap();
+        }
+    }
+
+    private void setFilter() {
+        if (mFilter.equals("0")){
+            mSetBWColorFilter();
+        } else if (mFilter.equals("1")){
+            mSetColorFilter();
             bitmap = blurRenderScript(((BitmapDrawable) getDrawable()).getBitmap(), 24);
+            setImageBitmap(bitmap);
         } else {
             bitmap = ((BitmapDrawable) getDrawable()).getBitmap();
+            setImageBitmap(bitmap);
         }
-        setImageBitmap(bitmap);
     }
 
     public void getAttributes(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -75,6 +89,8 @@ public class BlackWhiteImageView extends ImageView {
         mChangeTouchColor = a.getBoolean(R.styleable.BlackWhiteImageView_changeTouchColor, false);
         mChangeToBlacknWhite = a.getBoolean(R.styleable.BlackWhiteImageView_changeToBlacknWhite, false);
         mIsBlur = a.getBoolean(R.styleable.BlackWhiteImageView_isBlur, false);
+        mFilter = a.getString(R.styleable.BlackWhiteImageView_filter);
+        mTransform = a.getString(R.styleable.BlackWhiteImageView_transform);
         a.recycle();
     }
 
@@ -84,25 +100,18 @@ public class BlackWhiteImageView extends ImageView {
         }
     }
 
-    public void setBlacknWhite() {
-        if (mChangeToBlacknWhite) {
-            mSetColorFilter();
-        } else {
-            mSetBWColorFilter();
-        }
-    }
-
     public void mSetColorFilter() {
         ColorMatrix matrix = new ColorMatrix();
-        matrix.setSaturation(0);
+        matrix.setSaturation(1);
 
         ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
         setColorFilter(filter);
+
     }
 
     public void mSetBWColorFilter() {
         ColorMatrix matrix = new ColorMatrix();
-        matrix.setSaturation(1);
+        matrix.setSaturation(0);
 
         ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
         setColorFilter(filter);
@@ -131,6 +140,12 @@ public class BlackWhiteImageView extends ImageView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public void createCircularImageBitmap(){
+        RoundedBitmapDrawable roundDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        roundDrawable.setCircular(true);
+        setImageDrawable(roundDrawable);
     }
 
     private Bitmap RGB565toARGB888(Bitmap img) throws Exception {
